d99e1dd20912f09242fb4aaeac8eee5e6c222cb6
Hind Sarra <hind.bourad0619@gmail.com>
2023-07-11 18:59:53 +0200

    modif header

--- templates/header.php
+++ templates/header.php
@@ -1,35 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Garage-Parrot</title>
+  <title>Garage automobile Vincent PAROT</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  <link rel="stylesheet" href="assets/css/override-bootstrap.css">
+  <link rel="stylesheet" href="assets/css/bootstrap_override.css">
   <link rel="stylesheet" href="assets/css/style.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-
 </head>
 
 <body>
-  <div class="contenair">
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <div class="col-md-3 mb-2 mb-md-0">
-        <a href="index.php" class="d-inline-flex link-body-emphasis text-decoration-none">
-          <img src="assets/image/logo_horizontal.png" alt="Logo garage_auto" width="250">
-        </a>
-      </div>
-
-      <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+  <div class="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+      <a href="index.php" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+        <img src="assets/image/logo_horizontal" alt="Logo garage-auto" width="250">
+      </a>
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="#index.php" class="nav-link px-2 link-secondary">Acceuil</a></li>
+        <li><a href="#voitures.php" class="nav-link px-2 link-dark">Nos voitures</a></li>
+        <li><a href="#ajoutModif.php" class="nav-link px-2 link-dark">Ajout voitures</a></li>
+        <li><a href="#contact.php" class="nav-link px-2 link-dark">Contact</a></li>
+        <li><a href="#about.php" class="nav-link px-2 link-dark">A propos</a></li>
       </ul>
       <div class="col-md-3 text-end">
-        <button type="button" class="btn btn-outline-primary me-2">Login</button>
-        <button type="button" class="btn btn-primary">Sign-up</button>
+        <button type="button" class="btn btn-primary">Se connecter</button>
+      </div>
     </header>
-  </div>
