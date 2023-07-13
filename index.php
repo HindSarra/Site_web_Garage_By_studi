27ddfaba689984c5c20ca82430678a06193a9f96
Hind Sarra <hind.bourad0619@gmail.com>
2023-07-13 17:17:49 +0200

    fix error on index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
   <div class="col-10 col-sm-8 col-lg-6">
-    <img src="assets/uploads/img-acceuil.jpg" class="d-block mx-lg-auto img-fluid" alt="Logo voiture" width="350" loading="lazy">
+    <img src="assets/uploads/img-acceuil.jpg" class="d-block mx-lg-auto img-fluid" alt="Logo voiture" width="400" loading="lazy">
   </div>
   <div class="col-lg-6">
     <h1 class="display-5 fw-bold lh-1 mb-3">Voici nos voitures d'occassion on tres bonne etat</h1>
