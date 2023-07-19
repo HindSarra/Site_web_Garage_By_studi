0142728b1a8e2ef6219afb54d90b25261bdab5cf
Hind Sarra <hind.bourad0619@gmail.com>
2023-07-19 16:51:16 +0200

    add env var in pdo

--- bdd/config.php
+++ bdd/config.php
@@ -1,4 +1,11 @@
 <?php
-$dbname='';
-$host="";
-$pdo = new PDO('mysql:$dbname;host=localhost;charset=utf8mb4', 'root', '');
+$dbhost = getenv('DB_HOST') ?: '127.0.0.1';
+$dbport = getenv('DB_PORT') ?: '3306';
+$dbname = getenv('DB_NAME') ?: 'test';
+
+$dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname}";
+$username = getenv('DB_USERNAME') ?: 'test';
+$password = getenv('DB_PASSWORD') ?: 'test';
+
+$pdo = new PDO($dsn, $username, $password);
+?>
